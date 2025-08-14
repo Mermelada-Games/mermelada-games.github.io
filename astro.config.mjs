2ab2632121173f5d46d20f27a6c755d0a5e43637
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2025-08-14 22:47:01 +0200

    site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.mermeladagames.com/',
+  
   integrations: [react()],
 
   vite: {
