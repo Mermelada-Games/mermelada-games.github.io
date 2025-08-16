1f262c89ea56ad7b114055ab9cdaa4805eb7f87e
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2025-08-16 22:10:53 +0200

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.mermeladagames.com/',
-  
-  integrations: [react()],
+
+  integrations: [
+    react(),
+    partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()]
